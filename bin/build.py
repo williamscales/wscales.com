41e2db8cf2a72c93abe91852ca3b876fcc60da4d
William Scales <will@wscales.com>
2025-03-21 20:07:33 -0600

    Refactor build script to improve modularity and add new page rendering functionality (#3)
    * Refactor build script to improve modularity and add new page rendering functionality; introduce 'about.html' page and update layout with navigation links
    
    * Refactor get_page function to return both page content and title; update render_page to utilize new return structure for dynamic title rendering
    
    * Update README.md to clarify page structure, layout rendering, and development instructions; enhance build and deployment sections with detailed output paths and environment variables.
    
    * Add development server script and update README; include requirements for hot reloading
    
    * Update layout and content across multiple pages; adjust file paths for assets, enhance error messaging, and add personal branding to the index and about pages.
    
    * Remove navigation links from the default layout header; comment out the code for future reference.

--- bin/build.py
+++ bin/build.py
@@ -6,70 +6,88 @@ LAYOUT_ROOT = "layouts"
 PAGE_ROOT = "pages"
 STATIC_ROOT = "static"
 
-pages = {
-    "home": {
-        "title": "Home",
-        "path": "index.html",
-    },
-    "error": {
-        "title": "Error",
-        "path": "error.html",
-    },
-}
 
-layouts = {
-    "default": {
-        "path": "default.html",
-    },
-}
+def check_build_root() -> None:
+    """Delete the build root and create a new one."""
+    if os.path.exists(BUILD_ROOT):
+        print("Deleting build root...")
+        shutil.rmtree(BUILD_ROOT)
+    os.makedirs(BUILD_ROOT)
+    print("Build root created")
 
 
-def render_page(page, layout):
-    """Render a page from a layout and a page."""
-    page_path = os.path.join(PAGE_ROOT, page["path"])
-    layout_path = os.path.join(LAYOUT_ROOT, layout["path"])
-    with open(page_path, "r") as f:
+def get_layout(layout_path: str = "default.html") -> str:
+    """Get the layout content from a file."""
+    with open(os.path.join(LAYOUT_ROOT, layout_path), "r") as f:
+        return f.read()
+
+
+def get_pages() -> list[str]:
+    """Find all pages in the pages directory."""
+    return os.listdir(PAGE_ROOT)
+
+
+def get_page(page_path: str) -> tuple[str, str]:
+    """Get the page content from a file."""
+    with open(os.path.join(PAGE_ROOT, page_path), "r") as f:
         page_content = f.read()
-    with open(layout_path, "r") as f:
-        layout_content = f.read()
-    return layout_content.replace("{{page_content}}", page_content).replace(
-        "{{page_title}}", page["title"]
+        page_title = page_content.split("\n")[0]
+        page_content = "\n".join(page_content.split("\n")[1:])
+        return page_content, page_title
+
+
+def render_page(page_path: str, layout: str) -> str:
+    """Render a page from a layout and a page."""
+    page_content, page_title = get_page(page_path)
+    return layout.replace("{{page_content}}", page_content).replace(
+        "{{page_title}}", page_title
     )
 
 
-def check_build_root():
-    """Delete the build root and create a new one."""
-    if os.path.exists(BUILD_ROOT):
-        shutil.rmtree(BUILD_ROOT)
-    os.makedirs(BUILD_ROOT)
-
-
-def copy_static():
-    """Copy all files in the static directory to the build root."""
-    for item in os.listdir(STATIC_ROOT):
-        s = os.path.join(STATIC_ROOT, item)
-        d = os.path.join(BUILD_ROOT, item)
-        print(d)
-        if os.path.isfile(s):
-            shutil.copy2(s, d)
-        elif os.path.isdir(s):
-            shutil.copytree(s, d, dirs_exist_ok=True)
-
-
-def render_pages():
+def render_pages(pages: list[str], layout: str) -> None:
     """Render all pages and save them to the build root."""
-    for _, page in pages.items():
-        page_path = os.path.join(BUILD_ROOT, page["path"])
-        print(page_path)
-        with open(page_path, "w") as f:
-            f.write(render_page(page, layouts["default"]))
+    print("Rendering pages...")
+    for page_path in pages:
+        page_name = page_path.replace(".html", "")
+
+        if page_name == "index":
+            output_dir = BUILD_ROOT
+        else:
+            output_dir = os.path.join(BUILD_ROOT, page_name)
+        output_path = os.path.join(output_dir, "index.html")
+
+        print(output_path)
+
+        os.makedirs(output_dir, exist_ok=True)
+
+        with open(output_path, "w") as f:
+            f.write(render_page(page_path, layout))
 
 
-def build():
+def copy_static() -> None:
+    """Copy all files in the static directory to the build root."""
+    print("Copying static files...")
+    for item in os.listdir(STATIC_ROOT):
+        source_path = os.path.join(STATIC_ROOT, item)
+        destination_path = os.path.join(BUILD_ROOT, item)
+
+        print(destination_path)
+
+        if os.path.isfile(source_path):
+            shutil.copy2(source_path, destination_path)
+        elif os.path.isdir(source_path):
+            shutil.copytree(source_path, destination_path, dirs_exist_ok=True)
+
+
+def build() -> None:
     """Build the website."""
+    print("Building...")
     check_build_root()
-    render_pages()
+    layout = get_layout()
+    pages = get_pages()
+    render_pages(pages, layout)
     copy_static()
+    print("Build complete")
 
 
 if __name__ == "__main__":
